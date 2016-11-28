ab31be2012f5ddef5334578b8a72bdeaaeb4d915
Fernando <ft@bad-toro.com>
2016-11-28 12:14:09 -0500

    Latest

--- app/src/main/java/com/mac/training/fragmentcommunication/MainActivity.java
+++ app/src/main/java/com/mac/training/fragmentcommunication/MainActivity.java
@@ -5,11 +5,13 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements iComm{
 
     Fragment f1;
     SecondFragment f2;
+    TextView tV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +26,15 @@ public class MainActivity extends AppCompatActivity implements iComm{
         fT.add(R.id.forFirst,f1);
         fT.add(R.id.forSecond,f2);
         fT.commit();
+
+        tV = (TextView) findViewById(R.id.textView);
     }
 
     @Override
     public void setTextTo(String s) {
 
         f2.setIt(s);
+        tV.setText(s);
 
     }
 }
